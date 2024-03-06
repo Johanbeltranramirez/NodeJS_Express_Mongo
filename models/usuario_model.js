8b31dd3e0b87cafc09def2a591ba0620378064de
Johanbeltranramirez <johanframirez13@gmail.com>
2024-03-06 15:13:58 -0500

    Agregando el modelo usuario

--- models/usuario_model.js
+++ models/usuario_model.js
@@ -1,37 +1,25 @@
-const usuarios = require('./controllers/usuarios');
-
-const cursos = require('./controllers/cursos');
-
-const express = require('express');
-
 const mongoose = require('mongoose');
 
-// Conexion a la base de datos MongoDB
-
-mongoose.connect('mongodb://localhost:27017/userscoursesdb')
-
-    .then(() => console.log('Conectado a MongoDB...'))
-
-    .catch(err => console.error('No se pudo conectar con MongoDB:', err));
-
-// Middleware 
-
-const app = express();
-
-app.use(express.json());
-
-app.use(express.urlencoded({extended:true}));
-
-//end points (recursos)
-
-app.use('/api/usuarios', usuarios);
-
-app.use('/api/cursos', cursos);
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-
-    console.log('API REST Ok, y ejecutándose...');
-
+const usuarioSchema = new mongoose.Schema({
+    email: {
+        type : String,
+        required: true
+    },
+    nombre: {
+        type : String,
+        required : true
+    },
+    password: {
+        type : String,
+        required: true
+    },
+    estado: {
+        type: Boolean,
+        default: true
+    },
+    imagen: {
+        type: String,
+        required: false
+    }
 });
+module.exports = mongoose.model('Usuario', usuarioSchema);
